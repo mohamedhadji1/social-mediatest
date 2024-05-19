3affdd89bda8d7f69b3f413eb27d31e7ce1500d7
mohamedhadji1 <100348710+mohamedhadji1@users.noreply.github.com>
2024-05-19 22:08:59 +0100

    aaa

--- src/app/tools/profile/profile.component.ts
+++ src/app/tools/profile/profile.component.ts
@@ -14,9 +14,7 @@ export class ProfileComponent implements OnInit {
   auth: FirebaseTSAuth;
   storage: FirebaseTSStorage;
   selectedImage: File | null = null;
-  userEmail: string = '';
-  formSubmitted: boolean = false;
-  userJustSignedUp: boolean = false;
+
   constructor() {
     this.firestore = new FirebaseTSFirestore();
     this.auth = new FirebaseTSAuth();
@@ -25,28 +23,8 @@ export class ProfileComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.auth.getAuth().onAuthStateChanged(user => {
-      if (user) {
-        this.userEmail = user.email || '';  // Set the user email
-        console.log('User signed in:', user.uid, user.email);
-      } else {
-        console.log('No user is signed in.');
-      }
-    });
-    this.auth.getAuth().onAuthStateChanged(user => {
-      if (user) {
-        this.userJustSignedUp = true;
-        this.checkFormSubmission();
-      } else {
-        this.userJustSignedUp = false;
-      }
-    });
   }
-  checkFormSubmission() {
-    // Check local storage to see if the form has been submitted
-    const submitted = localStorage.getItem('formSubmitted');
-    this.formSubmitted = submitted === 'true';
-  }
+
   onImageSelected(imageInput: HTMLInputElement) {
     if (imageInput.files && imageInput.files.length > 0) {
       this.selectedImage = imageInput.files[0];
@@ -61,8 +39,7 @@ export class ProfileComponent implements OnInit {
     specializationInput: HTMLInputElement,
     labInput: HTMLInputElement,
     phoneInput: HTMLInputElement,
-    imageInput: HTMLInputElement,
-    aboutMeInput: HTMLTextAreaElement,
+    imageInput: HTMLInputElement
   ) {
     let firstName = firstNameInput.value;
     let lastName = lastNameInput.value;
@@ -71,7 +48,6 @@ export class ProfileComponent implements OnInit {
     let specialization = specializationInput.value;
     let lab = labInput.value;
     let phone = phoneInput.value;
-    let aboutMe = aboutMeInput.value;  // Get the value from the textarea
 
     const auth = this.auth.getAuth();
     if (auth !== null) {
@@ -96,13 +72,12 @@ export class ProfileComponent implements OnInit {
                   specialization: specialization,
                   lab: lab,
                   phone: phone,
-                  aboutMe: aboutMe,
                   imageUrl: downloadUrl,
                   role: 'chercheur'
                 },
                 onComplete: (docId) => {
                   alert("Profile Created");
-                  this.formSubmitted = true; // Set formSubmitted to true to hide the form
+                  this.resetForm(firstNameInput, lastNameInput, universityInput, emailInput, specializationInput, labInput, phoneInput, imageInput);
                 },
                 onFail: (err) => {
                   // Handle failure
@@ -124,8 +99,7 @@ export class ProfileComponent implements OnInit {
             },
             onComplete: (docId) => {
               alert("Profile Created");
-              this.formSubmitted = true;
-              localStorage.setItem('formSubmitted', 'true'); // Save submission state to local storage
+              this.resetForm(firstNameInput, lastNameInput, universityInput, emailInput, specializationInput, labInput, phoneInput, imageInput);
             },
             onFail: (err) => {
               // Handle failure
@@ -139,5 +113,25 @@ export class ProfileComponent implements OnInit {
       // Handle the case where auth is null
     }
   }
-}
 
+  resetForm(
+    firstNameInput: HTMLInputElement,
+    lastNameInput: HTMLInputElement,
+    universityInput: HTMLInputElement,
+    emailInput: HTMLInputElement,
+    specializationInput: HTMLInputElement,
+    labInput: HTMLInputElement,
+    phoneInput: HTMLInputElement,
+    imageInput: HTMLInputElement
+  ) {
+    firstNameInput.value = "";
+    lastNameInput.value = "";
+    universityInput.value = "";
+    emailInput.value = "";
+    specializationInput.value = "";
+    labInput.value = "";
+    phoneInput.value = "";
+    imageInput.value = "";
+    this.selectedImage = null;
+  }
+}
